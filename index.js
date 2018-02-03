83b8f46557106aaf64afe5bec039474f6d591840
Ginger Williams <gwilliams@Admins-MacBook-Pro-9.local>
2018-02-03 14:26:37 -0500

    bad test

--- index.js
+++ index.js
@@ -11,30 +11,18 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 }); 
 
-let people = {};
-
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    socket.on('join', function(name){
-        people[socket.id] = name; //create entry in 'people' with new user
-        socket.emit("update", "You have connected to the server.");
-        io.sockets.emit("update", name + " has joined the server.");
-        io.sockets.emit("update_people_list", people);
+    socket.on('chat message', function(msg){
+        //console.log('message: ' + msg);
+        //all but a certain socket
+        //socket.broadcast.emit('hi');
+        io.emit('chat message', msg);
     });
 
     socket.on('disconnect', function(){
-        console.log('user disconnected!');
-        if(people[socket.id] != ""){
-            io.sockets.emit("update", people[socket.id] + " has left the server.");
-            delete people[socket.id];
-            io.sockets.emit("update_people_list", people);
-        }
-    });
-
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.sockets.emit('chat message', people[socket.id], msg);
+        console.log('user disconnected');
     });
 });
 
