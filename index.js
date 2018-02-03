8cb49f7c967d4b98e938b05ba12b627e57f1f262
Ginger Williams <gwilliams@Admins-MacBook-Pro-9.local>
2018-02-02 22:30:55 -0500

    codeship test push

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-
-    
-    
 });
 
 // io.emit('some event', { for: 'everyone' });
