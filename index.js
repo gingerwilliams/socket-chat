59fa208daf0dc1dba450b354cb39071da777909c
Ginger Williams <gwilliams@Admins-MacBook-Pro-9.local>
2018-02-03 13:51:45 -0500

    my first node app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 // app.get('/', function(req, res){
 //   res.send('<h1>Hello world</h1>');
@@ -27,6 +28,9 @@ io.on('connection', function(socket){
 
 // io.emit('some event', { for: 'everyone' });
 
-http.listen(3000, function(){
+// http.listen(3000, function(){
+//     console.log('listening on *:3000');
+// });
+http.listen(port, function(){
     console.log('listening on *:3000');
 });
